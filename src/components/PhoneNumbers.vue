aaf4b00085492ba6adbaf66b9d9cf90b0a321c96
xelorabb <xelorabb@gmail.com>
2021-10-10 17:31:45 +0200

    v0.2.0 phone numbers part 2

--- src/components/PhoneNumbers.vue
+++ src/components/PhoneNumbers.vue
@@ -1,14 +1,17 @@
 <template>
+  <div class="my-10"><Random /></div>
   <div class="my-10"><Select /></div>
 </template>
 
 <script>
   import Select from './phone_numbers/Select.vue'
+  import Random from './phone_numbers/Random.vue'
 
   export default {
     name: 'PhoneNumbers',
     components: {
-      Select
+      Select,
+      Random
     }
   }
 </script>
---
+++ src/components/phone_numbers/Random.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>Is this your phone number? {{ random_number }}</div>
+  <div>
+    <button class="btn full" @click="gen_random_number()">No</button>
+    <button class="btn outline">Yes</button>
+  </div>
+  <hr class="mt-10"/>
+</template>
+
+<script>
+export default {
+  name: 'Random',
+  created() {
+    this.gen_random_number()
+  },
+  data() {
+    return {
+      random_number: "",
+    }
+  },
+  methods: {
+    gen_random_number: function() {
+      this.random_number = Math.floor(
+        Math.random() * 9000000000) + 1000000000
+    }
+  }
+}
+</script>

e05bd1d59c7deab0fdee715b830763d0c096909a
xelorabb <xelorabb@gmail.com>
2021-10-10 17:34:31 +0200

    v0.3.0 phone numbers part 3

--- src/components/PhoneNumbers.vue
+++ src/components/PhoneNumbers.vue
@@ -1,4 +1,5 @@
 <template>
+  <div class="my-10"><Counter /></div>
   <div class="my-10"><Random /></div>
   <div class="my-10"><Select /></div>
 </template>
@@ -6,12 +7,14 @@
 <script>
   import Select from './phone_numbers/Select.vue'
   import Random from './phone_numbers/Random.vue'
+  import Counter from './phone_numbers/Counter.vue'
 
   export default {
     name: 'PhoneNumbers',
     components: {
       Select,
-      Random
+      Random,
+      Counter
     }
   }
 </script>
---
+++ src/components/phone_numbers/Counter.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>Set your phone number: {{number}}</div>
+  <div>
+    <button class="btn full" @click="restart()">Restart</button>
+    <button class="btn outline" @click="set()">Set</button>
+    <button class="btn link">Ok</button>
+  </div>
+  <hr class="mt-10"/>
+</template>
+
+<script>
+export default {
+  name: 'Counter',
+  created() {
+    this.restart()
+    this.createInterval()
+  },
+  data() {
+    return {
+      pos: 0,
+      count: 0,
+      number: "",
+      itime: 0,
+      interval: null
+    }
+  },
+  methods: {
+    restart: function() {
+      this.pos = 0
+      this.itime = 200
+      this.number = new Array(10).fill(0).join('')
+      this.createInterval()
+    },
+
+    set: function() {
+      this.pos++
+      this.itime -= 20
+      this.createInterval()
+    },
+
+    createInterval: function() {
+      clearInterval(this.interval)
+
+      this.interval = setInterval(() => {
+        this.number = this.number.replace(
+          /./g, (c, i) => i == this.pos ? this.count : c)
+
+        this.count++
+        if(this.count > 9) this.count = 0
+
+      }, this.itime)
+    }
+  }
+}
+</script>
